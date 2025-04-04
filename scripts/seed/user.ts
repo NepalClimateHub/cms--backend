e17c2d466949aaee151a2b9780f3b2b261cfec6c
Mukesh Kumar Chaudhary <mukezhz@gmail.com>
2025-04-04 14:56:31 +0545

    feat: add opportunity module and update event model

--- scripts/seed/user.ts
+++ scripts/seed/user.ts
@@ -7,19 +7,28 @@ const seedSuperAdmin = async () => {
   const pass = "Test@123";
   const hasedPW = await hash(pass, 10);
 
-  const superAdmin: Prisma.UserCreateInput = {
-    fullName: "Super Admin",
-    email: "avash700@gmail.com",
-    password: hasedPW,
-    isAccountVerified: true,
-    isSuperAdmin: true,
-    gender: "Male",
-  };
-
-  await prisma.user.create({
-    data: {
-      ...superAdmin,
+  const superAdmin: Prisma.UserCreateInput[] = [
+    {
+      fullName: "Super Admin",
+      email: "avash700@gmail.com",
+      password: hasedPW,
+      isAccountVerified: true,
+      isSuperAdmin: true,
+      gender: "Male",
     },
+    {
+      fullName: "Mukesh Kumar Chaudhary",
+      email: "mukezhz@gmail.com",
+      password: hasedPW,
+      isAccountVerified: true,
+      isSuperAdmin: true,
+      gender: "Male",
+    },
+  ];
+
+  await prisma.user.createMany({
+    data: superAdmin,
+    skipDuplicates: true,
   });
   console.log("Super admin seeded!");
 };
