6364b76c1c1c098b026f5ddfafec6ca82b29bdca
Mukesh Kumar Chaudhary <mukezhz@gmail.com>
2025-04-04 12:22:07 +0545

    fix: update seed script and package.json for consistency

--- scripts/seed/user.ts
+++ scripts/seed/user.ts
@@ -1,19 +1,19 @@
-import { PrismaClient, Prisma } from '@prisma/client';
-import { hash } from 'bcrypt';
+import { PrismaClient, Prisma } from "@prisma/client";
+import { hash } from "bcrypt";
 
 const prisma = new PrismaClient();
 
 const seedSuperAdmin = async () => {
-  const pass = 'Test@123';
+  const pass = "Test@123";
   const hasedPW = await hash(pass, 10);
 
   const superAdmin: Prisma.UserCreateInput = {
-    fullName: 'Super Admin',
-    email: 'avash700@gmail.com',
+    fullName: "Super Admin",
+    email: "avash700@gmail.com",
     password: hasedPW,
     isAccountVerified: true,
     isSuperAdmin: true,
-    gender: 'Male',
+    gender: "Male",
   };
 
   await prisma.user.create({
@@ -21,7 +21,7 @@ const seedSuperAdmin = async () => {
       ...superAdmin,
     },
   });
-  console.log('Super admin seeded!');
+  console.log("Super admin seeded!");
 };
 
 (() => {
