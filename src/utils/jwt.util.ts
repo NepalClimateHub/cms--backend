40bcc018a3ce28911c2bdbd3d1f2add9b81b6a05
Saroj Dahal <isarojdahal@gmail.com>
2025-10-12 14:45:08 +0545

    create: utils for email and jwt

---
+++ src/utils/jwt.util.ts
@@ -0,0 +1,10 @@
+import { JwtService } from "@nestjs/jwt";
+
+export const getJWTTokenForEmailVerification = (email: string) => {
+    const jwtService = new JwtService();
+
+    return jwtService.sign({email}, {
+        expiresIn: "10m",
+        secret:"temp-secret-key"
+      });
+}
